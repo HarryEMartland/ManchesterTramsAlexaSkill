434c4baad5ada5c000344f1ee90e6e02e298271e
martlandh <HarryMartland@Rentalcars.com>
2016-12-14 21:45:25 +0000

    fixed tests not resetting mock

--- src/test/EndToEndTests.ts
+++ src/test/EndToEndTests.ts
@@ -8,10 +8,10 @@ import {HttpClient} from "../main/HttpClient";
 let files = fs.readdirSync("./src/test/resources");
 
 process.env.response_ending = "That's all from the trams.";
-process.env.alexa_skill_application_id ="5739j3k4j332";
+process.env.alexa_skill_application_id = "5739j3k4j332";
 
 let request = {
-    session:{application:{applicationId:"5739j3k4j332"}}
+    session: {application: {applicationId: "5739j3k4j332"}}
 } as Request;
 
 describe("End To End Tests", function () {
@@ -21,18 +21,22 @@ describe("End To End Tests", function () {
 
             mockHttpClient(testJson.mockResponse);
 
-            ManMetHandler.handler(request, null, function (error:any, result:any) {
+            ManMetHandler.handler(request, null, function (error: any, result: any) {
                 let actual = JSON.stringify(result);
                 let expected = JSON.stringify(testJson.expectedResponse);
                 assert.equal(actual, expected);
                 done();
             });
         })
+    });
+
+    afterEach(function () {
+        sinon.restore(HttpClient);
     })
 });
 
-function mockHttpClient( mockResponse:any){
-    sinon.stub(HttpClient, "get", function (request: Request) :Promise<string>{
+function mockHttpClient(mockResponse: any) {
+    sinon.stub(HttpClient, "get", function (): Promise<string> {
         return Promise.resolve(JSON.stringify(mockResponse))
     });
 }
